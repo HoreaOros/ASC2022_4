ae9206a3a22c88c6779b46c82d48f33e8e704b8a
Horea Oros <horea.oros@gmail.com>
2022-10-11 12:56:54 +0300

    Add project files.

---
+++ 10_11/10_11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 256;
+            int pad = 8;
+
+
+
+            for (int i = 0; i < 256; i++)
+            {
+                ShowPad(i, pad);
+            }
+            
+        }
+
+        private static void ShowPad(int no, int pad)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (no != 0)
+            {
+                stack.Push(no % 2);
+                no = no / 2;
+            }
+            int z;
+            z = pad - stack.Count;
+            for (int i = 0; i < z; i++)
+            {
+                stack.Push(0);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
